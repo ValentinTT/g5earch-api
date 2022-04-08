4c296faee01344d9c8f7595c6f664a7b32cc6ce1
ValentinTT <valentintapiatorti@gmail.com>
2022-04-08 10:27:02 -0300

    first commit

---
+++ src/main/java/com/example/g5earchapi/G5earchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.g5earchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G5earchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G5earchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/g5earchapi/G5earchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g5earchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G5earchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
